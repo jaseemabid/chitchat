1a6e1add5bc05e801672c4c79ca07f2175f54fff
Jaseem Abid <jaseemabid@gmail.com>
2015-08-11 01:39:35 +0530

    Can talk both ways, but no ack

--- talk.ml
+++ talk.ml
@@ -7,7 +7,7 @@ let rec receiver buffer r w =
   >>= function
   | `Eof -> return ()
   | `Ok len ->
-     let _ = Writer.write w "OK\n" in
+     (* let _ = Writer.write w "OK\n" in *)
      let _ = Writer.flushed w in
      let _ = print_endline (" ✉ " ^ (String.sub buffer 0 len)) in
      receiver buffer r w
@@ -23,7 +23,7 @@ let rec sender buffer peer =
      let _ = Writer.flushed peer in
      sender buffer peer
 
-(* Callback function for new TCP client connections *)
+(* Callback function for new TCP connections from clients *)
 let callback _addr r w =
   print_endline "Client connected";
   let sender_buffer = String.create (16 * 1024) in
@@ -35,14 +35,15 @@ let callback _addr r w =
   every time a client connects. *)
 let server port () =
   print_endline "Starting up the server";
-  let host_and_port =
-    Tcp.Server.create ~on_handler_error:`Raise (Tcp.on_port port) callback
-  in
-  ignore (host_and_port : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t)
+  let where_to_listen = (Tcp.on_port port) in
+  ignore  (Tcp.Server.create ~on_handler_error:`Raise where_to_listen callback);
+  never ()
 
-
-let client () =
-  print_endline "Starting up the client"
+let client port () =
+  print_endline "Starting up the client";
+  let target = (Tcp.to_host_and_port "127.0.0.1" port) in
+  ignore (Tcp.with_connection target callback);
+  never ()
 
 (* Unable to use the stdlib variant due to some build issue *)
 let default x opt =
@@ -60,8 +61,8 @@ let command =
                   +> flag "-p" (optional int) ~doc:"Port to bind/connect server")
     (fun s p () ->
      match s with
-     | true -> server (default 8765 p) ()
-     | false -> client ())
+     | true -> let _ = server (default 8765 p) () in ()
+     | false -> let _ = client (default 8765 p) () in ())
 
 (* Call command and then start the scheduler *)
 let () =
